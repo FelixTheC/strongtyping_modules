f735196552cfbe3623ba4bc92eb2013c03395893
FelixTheC <felixeisenmenger@gmx.net>
2020-08-17 16:09:25 +0200

    [UPD] missing requirement

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from Cython.Build import cythonize
 
 
 setup(name='strongtyping_modules',
-      version='0.0.1b',
+      version='0.0.2b',
       description='Utils for strongtyping.',
       author='Felix Eisenmenger',
       author_email='fberndt87@gmail.com',
       ext_modules=cythonize('strongtyping_modules.pyx'),
       zip_safe=False,
+      install_requires=('Cython==0.29.21', )
       )
