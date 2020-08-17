d52a79829501ea104c2450ddcf4cb9e0807d07a7
FelixTheC <felixeisenmenger@gmx.net>
2020-08-17 21:27:15 +0200

    [UPD] include package files

--- setup.py
+++ setup.py
@@ -7,17 +7,20 @@ try:
     ext_modules = cythonize('strongtyping_modules/strongtyping_modules.pyx',
                             compiler_directives={'language_level': 3, 'embedsignature': True})
 except (ImportError, NameError):
-    ext_modules = None
+      ext_modules = None
 
 
 setup(name='strongtyping_modules',
-      version='0.0.4.3b',
+      version='0.0.4.4b',
       description='Utils for strongtyping.',
       author='Felix Eisenmenger',
       author_email='fberndt87@gmail.com',
       ext_modules=ext_modules,
       zip_safe=False,
       packages=find_packages(),
+      package_data={
+            '': ('*.pyx', '*.c'),
+      },
       classifiers=[
             'Programming Language :: Python :: 3',
             'License :: OSI Approved :: MIT License',
