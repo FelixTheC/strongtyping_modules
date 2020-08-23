4b8cf88bd0d80b235e4a1960806ea72824e87503
FelixTheC <felixeisenmenger@gmx.net>
2020-08-23 19:58:36 +0200

    [UPD] easier access to install function

--- strongtyping_modules/__init__.py
+++ strongtyping_modules/__init__.py
@@ -4,3 +4,8 @@
 @created: 17.08.20
 @author: felix
 """
+from strongtyping_modules.install import install
+
+__all__ = (
+    'install',
+)
--- strongtyping_modules/install.py
+++ strongtyping_modules/install.py
@@ -12,7 +12,3 @@ def install():
     module = pathlib.Path(__file__).parent.joinpath('strongtyping_modules.pyx')
     p = subprocess.Popen(['cythonize', '-a', '-i', str(module)])
     p.wait()
-
-
-if __name__ == '__main__':
-    install()
